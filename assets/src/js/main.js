1cad82e7efc76a80d450a1dafbb48bfc451fcc34
hoang pham <hoanggghck@gmail.com>
2021-10-11 21:41:44 +0700

    update project

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -5,8 +5,31 @@
         $('.cover-slider').slick({
             slidesToShow:1,
             arrows:true,
-          });
+            rows:0
+        });
+
+        $('.nav-list .nav-link').on('click', function(){
+            $('.nav-list .nav-link').removeClass('active');
+            $(this).addClass('active');
+        });
     });
+
+    $(window).on('scroll', function(event) {
+        scroll_animate();
+    });
+
+    function scroll_animate() {
+        let currentScrollTop = $(window).scrollTop();
+
+        $('.animation').each(function(index, el) {
+            let elementOffset = $(this).offset();
+            let positionDelay = Math.min( $(this).height() / 3, $(window).height() / 2 );
+
+            if( ( currentScrollTop + $(window).height() - positionDelay ) > elementOffset.top ) {
+                $(this).addClass('animated');
+            }
+        });
+    }
 
     
 }(jQuery));
